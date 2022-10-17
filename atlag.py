f7ea88f3ba2392d7aabe371bdead8ec2d3b90c0b
szt2bertam <szt2bertam@vasvari.org>
2022-10-17 11:36:29 +0200

    2 feladat nincs meg kesz

--- atlag.py
+++ atlag.py
@@ -18,10 +18,10 @@ def feladat02_osszeg() -> int:
 # Határozza meg a páratlan számok átlagát három tizetedes jegy pontossággal!
 def feladat03_osszeg() -> int:
     szamok = [2, 3, 4, 7, 1, 8, 6, 1, 2, 3, 7, 2, 2, 9, 6, 6]
-    atlag = 0
+    atlag = sum(szamok) / len(szamok)
     for szam in szamok:
-        if szam%2 > 0:
-            atlag = atlag + sum(szam) / len(szam)
+        if szam % 2 == 1:
+            atlag = atlag + szam
     return round(atlag,3)
 
 # 4. feladat
@@ -31,4 +31,6 @@ def feladat03_osszeg() -> int:
 def feladat04_osszeg() -> int:
     szamok1 = [2, 3, 4, 6, 1, 8, 6, 1]
     szamok2 = [3, 2, 4, 1, 5, 4, 7, 0]
+    szamok1atlag = sum(szamok1)/len(szamok1)
+
     return 0
